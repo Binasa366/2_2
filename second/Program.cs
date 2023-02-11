44473a7843ff751a0f21e36d4526d9d3a14e2fb4
Binasa366 <sabinajamolova2003@gmail.com>
2023-02-11 22:57:09 +0500

    sth

---
+++ second/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c =Convert.ToInt32(Console.ReadLine());
+int max = a;
+while (b > max)
+{
+    max = b;
+
+}
+if (c > max)
+{
+    max = c;
+}
+else
+{
+    max = a;
+}
+
+Console.WriteLine("Максимальным из трех чисел является " + max);
